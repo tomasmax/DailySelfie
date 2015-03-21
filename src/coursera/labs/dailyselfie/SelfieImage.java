1299cb18735a8af5266ab8fd489a5a40d0db6664
Tomas Madariaga <critimass@gmail.com>
2015-03-21 21:02:16 +0100

    SelfieImage class created

---
+++ src/coursera/labs/dailyselfie/SelfieImage.java
@@ -0,0 +1,57 @@
+package coursera.labs.dailyselfie;
+
+import coursera.labs.dayliselfie.provider.SelfieContract;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+
+public class SelfieImage {
+	private int _id;
+	private String name;
+	private String path;
+	private String thumbPath;
+	private Bitmap bmp;
+	
+	public static SelfieImage fromCursor(Cursor cursor) {
+		SelfieImage selfie = new SelfieImage();
+		
+		selfie.setId(cursor.getInt(cursor.getColumnIndex(SelfieContract.SELFIE_COLUMN_ID)));
+		selfie.setPath(cursor.getString(cursor.getColumnIndex(SelfieContract.SELFIE_COLUMN_PATH)));
+		selfie.setName(cursor.getString(cursor.getColumnIndex(SelfieContract.SELFIE_COLUMN_NAME)));
+		return selfie;
+	}
+	
+	public int getId() {
+		return _id;
+	}
+	public void setId(int id) {
+		this._id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public Bitmap getBmp() {
+		return bmp;
+	}
+	public void setBmp(Bitmap bmp) {
+		this.bmp = bmp;
+	}
+
+	public String getThumbPath() {
+		return thumbPath;
+	}
+
+	public void setThumbPath(String thumbPath) {
+		this.thumbPath = thumbPath;
+	}
+	
+}
+
